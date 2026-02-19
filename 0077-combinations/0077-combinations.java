cdbdeb61e600f4e1fa71b824133ce19542d606bc
PardivReddy <pardivreddy0410@gmail.com>
2026-02-19 17:55:24 +0530

    Time: 18 ms (81.9%), Space: 98.1 MB (66.06%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(n,k,1,new ArrayList<>());
+        return res;
+    }
+    void backtrack(int n,int k,int start,List<Integer> curr){
+        if(curr.size()==k){
+            res.add(new ArrayList<>(curr));
+            return ;
+        }
+        for(int i=start;i<=n;i++){
+            curr.add(i);
+            backtrack(n,k,i+1,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
