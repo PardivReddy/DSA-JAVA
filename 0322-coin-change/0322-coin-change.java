1ea7691d0d9167b8768746c5c77dadd50cf7fd1a
PardivReddy <pardivreddy0410@gmail.com>
2026-02-27 00:44:48 +0530

    Time: 15 ms (82.52%), Space: 46.2 MB (84.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,17 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0){
-            return 0;
-        }
-        Queue<Integer> q =new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        q.offer(0);
-        visited.add(0);
-        int steps=0;
-        while(!q.isEmpty()){
-            int size=q.size();
-            steps++;
-            for(int i=0;i<size;i++){
-                int curr=q.poll();
-                
-                for(int coin:coins){
-                    int next=curr+coin;
-                    if(next==amount){
-                        return steps;
-                    }
-                    if(next<amount && !visited.contains(next)){
-                    visited.add(next);
-                    q.offer(next);
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin>=0){
+                    dp[i]=Math.min(dp[i],1+dp[i-coin]);
                 }
-                }
-                
             }
         }
-        return -1;
+        return dp[amount ]>amount ? -1:dp[amount];
+
     }
+
 }
