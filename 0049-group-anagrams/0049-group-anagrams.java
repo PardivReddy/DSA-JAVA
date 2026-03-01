f5bb0270c4a8dd49b6f5bb49d43a376d04ee5268
PardivReddy <pardivreddy0410@gmail.com>
2026-03-02 01:44:41 +0530

    Time: 20 ms (15.58%), Space: 50.3 MB (21.85%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            int[] count = new int[26];
+
+            for (char c : word.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int c : count) {
+                sb.append("#").append(c);
+            }
+
+            String key = sb.toString();
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
