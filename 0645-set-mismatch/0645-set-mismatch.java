5e86ce98e0b03337be875ebea9e083dc4cf4144b
PardivReddy <pardivreddy0410@gmail.com>
2026-03-22 22:18:01 +0530

    Time: 3 ms (65.22%), Space: 47.5 MB (85.98%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        List<Integer> res=new ArrayList<>();
+        /*List<Integer> res=new ArrayList<>();
         Map<Integer,Integer> map=new HashMap<>();
         int duplicate=-1,missing=-1;
         int count=0;
@@ -15,6 +15,25 @@ class Solution {
                 missing=i;
             }
         }
-        return new int[]{duplicate,missing};
+        return new int[]{duplicate,missing};*/
+        int i=0;
+        int n=nums.length;
+        while(i<n){
+            int ci=nums[i]-1;
+            if(nums[i]!=nums[ci]){
+                int temp=nums[i];
+                nums[i]=nums[ci];
+                nums[ci]=temp;
+            }else{
+                i++;
+            }
+
+        }
+        for(int j=0;j<n;j++){
+            if(nums[j]!=j+1){
+                return new int[]{nums[j],j+1};
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
